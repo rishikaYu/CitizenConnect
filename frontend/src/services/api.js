0c85d3d142a14abcec9273d7034cb62cede04217
Rishika <rakunuru@wpi.edu>
2025-11-03 13:39:33 -0500

    Update backend URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://citizenconnect-9.onrender.com/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL;
+
 
 const api = axios.create({
   baseURL: API_BASE_URL,
